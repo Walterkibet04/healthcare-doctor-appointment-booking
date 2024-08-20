10d2727385d3be6babb7461f263e8a2f0c15ad71
Walter kibet <kibetwalter726@gmail.com>
2024-08-20 15:24:37 +0300

    create user and save on the database

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -1,44 +1,44 @@
-// "use server";
+"use server";
 
-// import { ID, InputFile, Query } from "node-appwrite";
+import { ID, Query } from "node-appwrite";
 
-// import {
-//   BUCKET_ID,
-//   DATABASE_ID,
-//   ENDPOINT,
-//   PATIENT_COLLECTION_ID,
-//   PROJECT_ID,
-//   databases,
-//   storage,
-//   users,
-// } from "../appwrite.config";
-// import { parseStringify } from "../utils";
+import {
+  BUCKET_ID,
+  DATABASE_ID,
+  ENDPOINT,
+  PATIENT_COLLECTION_ID,
+  PROJECT_ID,
+  databases,
+  storage,
+  users,
+} from "../appwrite.config";
+import { parseStringify } from "../utils";
 
-// // CREATE APPWRITE USER
-// export const createUser = async (user: CreateUserParams) => {
-//   try {
-//     // Create new user -> https://appwrite.io/docs/references/1.5.x/server-nodejs/users#create
-//     const newuser = await users.create(
-//       ID.unique(),
-//       user.email,
-//       user.phone,
-//       undefined,
-//       user.name
-//     );
+// CREATE APPWRITE USER
+export const createUser = async (user: CreateUserParams) => {
+  try {
+    // Create new user -> https://appwrite.io/docs/references/1.5.x/server-nodejs/users#create
+    const newUser = await users.create(
+      ID.unique(),
+      user.email,
+      user.phone,
+      undefined,
+      user.name
+    );
 
-//     return parseStringify(newuser);
-//   } catch (error: any) {
-//     // Check existing user
-//     if (error && error?.code === 409) {
-//       const existingUser = await users.list([
-//         Query.equal("email", [user.email]),
-//       ]);
+    return parseStringify(newUser);
+  } catch (error: any) {
+    // Check existing user
+    if (error && error?.code === 409) {
+      const documents = await users.list([
+        Query.equal("email", [user.email]),
+      ]);
 
-//       return existingUser.users[0];
-//     }
-//     console.error("An error occurred while creating a new user:", error);
-//   }
-// };
+      return documents?.users[0];
+    }
+    console.error("An error occurred while creating a new user:", error);
+  }
+};
 
 // // GET USER
 // export const getUser = async (userId: string) => {
